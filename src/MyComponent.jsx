28749db88bbff661657135d27c997bede18b9636
chaminack <thiago.sw35@gmail.com>
2024-11-17 19:05:58 -0300

    Implementado mais casos de uso de useState() hook

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,7 +5,10 @@ function MyComponent(){
     const [name, setName] = useState("Visitante");
     const [age, setAge] = useState(0);
     const [isEmployed, setIsEmployed] = useState(false);
-
+    const [quantity, setQuantity] = useState(1);
+    const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("Delivery");
 
     const updateName = () => {
         setName("Bob Esponja");
@@ -19,9 +22,30 @@ function MyComponent(){
         setIsEmployed(!isEmployed);
     }
 
+    function handleNameChange(event){
+        setName(event.target.value);
+    }
+
+    function handleQuantityChange(event){
+        setQuantity(event.target.value)
+    }
+    
+    function handleCommentChange(event){
+        setComment(event.target.value)
+    }
+
+    function handlePaymentChange(event){
+        setPayment(event.target.value)
+    }
+
+    function handleShippingChange(event){
+        setShipping(event.target.value)
+    }
+
     return(<div>
 
         <p>Nome: {name}</p>
+        <input value={name} onChange={handleNameChange}></input>
         <button onClick ={updateName}>Set Name</button>
 
         <p>Idade: {age}</p>
@@ -29,6 +53,35 @@ function MyComponent(){
 
         <p>Empregado: {isEmployed ? "Empregado" : "Desempregado"}</p>
         <button onClick ={toggleEmployedStatus}>Alterar Status</button>
+
+        <p>Quantity: {quantity}</p>
+        <input value={quantity} onChange={handleQuantityChange} type="number"></input>
+
+        <p></p>
+        <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery instructions"></textarea>
+        <p>Comment: {comment}</p>
+
+        <select value={payment} onChange={handlePaymentChange}>
+            <option value="">Selecione uma opção</option>
+            <option value="Visa">Visa</option>
+            <option value="Mastercard">Mastercard</option>
+            <option value="Elo">Elo</option>
+        </select>
+        <p>Payment: {payment}</p>
+
+        <label>
+            <input type="radio" value="Pick Up"
+                checked={shipping === "Pick Up"}
+                onChange={handleShippingChange}/>
+            Pick Up
+        </label>
+        <label>
+        <input type="radio" value="Delivery"
+                checked={shipping === "Delivery"}
+                onChange={handleShippingChange}/>
+            Delivery
+        </label>
+        <p>Shipping: {shipping}</p>
     </div>)
 
 }
